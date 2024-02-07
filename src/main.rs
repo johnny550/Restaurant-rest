bd21367d846efec6ddd842b9ed2bc58a34bac094
johnny550 <sergeijohnny@gmail.com>
2024-02-07 16:40:37 +0900

    moduling
    Create module "server". move server logic in there

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,12 @@
-mod ep_test;
+#[cfg(test)]
+mod tests;
+mod server;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use rand::Rng;
-use std::collections::{HashMap, HashSet};
-use std::sync::{Arc, Mutex};
+use crate::server::{Order, RestaurantApp};
+use actix_web::{http, web, App, HttpResponse, HttpResponseBuilder, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 
-#[derive(Hash, Eq, PartialEq, Debug, Clone, Serialize)]
-struct Order {
-    item: String,
-    table_number: u32,
-    cook_time: u32,
-}
-
-#[derive(Clone)]
-struct RestaurantApp {
-    orders: Arc<Mutex<HashMap<u32, HashSet<Order>>>>,
-}
-
-impl RestaurantApp {
-    fn new() -> Self {
-        RestaurantApp {
-            orders: Arc::new(Mutex::new(HashMap::new())),
-        }
-    }
-
-    fn add_order(&self, item: String, table_number: u32) {
-        println!("Adding order");
-        let cook_time = rand::thread_rng().gen_range(5..=15);
-        let order = Order {
-            item,
-            table_number,
-            cook_time,
-        };
-
-        let mut orders = self.orders.lock().unwrap();
-        orders
-            .entry(table_number)
-            .or_insert_with(HashSet::new)
-            .insert(order);
-    }
-
-    fn remove_order(&self, item: &str, table_number: u32) {
-        let mut orders = self.orders.lock().unwrap();
-        if let Some(table_orders) = orders.get_mut(&table_number) {
-            table_orders.retain(|order| order.item != item);
-            if table_orders.is_empty() {
-                orders.remove(&table_number);
-            }
-        }
-    }
-
-    fn query_all_orders(&self, table_number: u32) -> Vec<Order> {
-        let orders = self.orders.lock().unwrap();
-        orders.get(&table_number).cloned().unwrap_or_default().into_iter().collect()
-    }
-
-    fn get_order(&self, item: &str, table_number: u32) -> Option<Order> {
-        let orders = self.orders.lock().unwrap();
-        orders
-            .get(&table_number)
-            .and_then(|table_orders| table_orders.iter().find(|order| order.item == item).cloned())
-    }
-}
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct OrderRequest{
     item: String,
     table_number: u32,
@@ -80,7 +22,7 @@ async fn add_order(data: web::Data<RestaurantApp>, info: web::Json<OrderRequest>
 }
 
 async fn remove_order(data: web::Data<RestaurantApp>, info: web::Json<OrderRequest>) -> impl Responder {
-    println!("removing order {:?}", info);
+    println!("Removing order {:?}", info);
     let OrderRequest{item, table_number} = info.into_inner();
     data.remove_order(&item, table_number);
     HttpResponse::Ok().body("Order removed successfully.")
@@ -91,6 +33,12 @@ async fn query_all_orders(data: web::Data<RestaurantApp>, info: web::Json<u32>) 
     let table_number = info.into_inner();
     let orders = data.query_all_orders(table_number);
     HttpResponse::Ok().json(orders)
+
+    // MOCK DATA
+    // let mut j = vec![];
+    // j.push(Order{item: "tiepp".to_string(), table_number: 4, cook_time: 2});
+    // j.push(Order{item: "couscous".to_string(), table_number: 2, cook_time:2});
+    // HttpResponse::Ok().json(j)
 }
 
 async fn get_order(data: web::Data<RestaurantApp>, info: web::Json<OrderRequest>) -> impl Responder {
@@ -107,7 +55,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(app.clone()))
-            .route("/test_endpoint", web::get().to(ep_test::test_endpoint))
+            // .route("/test_endpoint", web::get().to(ep_test::test_endpoint))
             .route("/add_order", web::post().to(add_order))
             .route("/remove_order", web::post().to(remove_order))
             .route("/query_all_orders", web::get().to(query_all_orders))
